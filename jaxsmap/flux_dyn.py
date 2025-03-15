e75ee1d4ffcc5bfa75cb1f16b25bca51ffcb84a7
Kai Ikuta <47920265+KaiIkuta@users.noreply.github.com>
2025-03-15 15:33:57 +0900

    Update flux_dyn.py

--- jaxsmap/flux_dyn.py
+++ jaxsmap/flux_dyn.py
@@ -1,1 +1,16 @@
 #To be implemented until mid March
+
+
+class flux():
+
+    def __init__(self, params):
+        self.params = params
+
+    def area(self):
+        area = 0.
+        return area
+
+    def rel_flux(self):
+        rel_flux = 1.
+        return rel_flux
+    
