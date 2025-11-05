e99b712eb26716f4cc08498019e18c91dbe635a5
Kai Ikuta <47920265+KaiIkuta@users.noreply.github.com>
2025-11-05 12:49:38 +0900

    Update coordinate.py

--- jaxsmap/coordinate.py
+++ jaxsmap/coordinate.py
@@ -1,1 +1,17 @@
+import jax
+import jax.numpy as jnp
+from jax import jit
 
+incl = 60.
+
+@jit
+def x_cart(phi, lam):
+  return jnp.cos(phi)[:, None] * jnp.sin(lam)[None, :]
+
+@jit
+def y_cart(phi, lam):
+  return jnp.sin(incl) * jnp.sin(phi)[:, None] - jnp.cos(incl) * jnp.cos(phi)[:, None] * jnp.cos(lam)[None, :]
+
+@jit
+def z_cart(phi, lam):
+  return jnp.cos(incl) * jnp.sin(phi)[:, None] + jnp.sin(incl) * jnp.cos(phi)[:, None] * jnp.cos(lam)[None, :]
