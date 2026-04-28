a09b35d18652bf7de9c30cb7442ba29fa6218f62
Kai Ikuta <47920265+KaiIkuta@users.noreply.github.com>
2026-04-28 16:15:08 +0900

    Update coord.py

--- src/jaxsmap/coord.py
+++ src/jaxsmap/coord.py
@@ -19,6 +19,17 @@ def cart(phi, lam, incl):
     return x_cart(phi, lam), y_cart(phi, lam, incl), z_cart(phi, lam, incl)
 
 @jit
+def spherical_grid(n_lat, n_lon):
+    lat_edges = jnp.linspace(-1.0, 1.0, n_lat + 1)
+    lats = jnp.arcsin((lat_edges[:-1] + lat_edges[1:]) / 2.0)
+    dlat = jnp.diff(jnp.arcsin(lat_edges))
+    dlam = jnp.pi * 2. / n_lon
+    lons = jnp.linspace(-jnp.pi + dlam/2.0, jnp.pi - dlam/2.0, n_lon)
+    lat_grid, lon_grid = jnp.meshgrid(lats, lons, indexing='ij')
+    areas = jnp.full_like(lat_flat, 2.0/n_lat* dlam)
+    return lat_grid.ravel(), lon_grid.ravel(), areas
+
+@jit
 def vlos(phi, lam, vsini):
     return vsini * jnp.cos(phi) * jnp.sin(lam)
 
